8ae06dd50ecf8265cbbb60b25b6ea349a8a284bd
Savila Saraiva <saraivasvfarias@gmail.com>
2019-04-25 21:38:16 -0300

    Alter server

--- servidor.py
+++ servidor.py
@@ -8,14 +8,13 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Atribui um endereço IP e um número de porta ao socket
 serverSocket.bind(('', 12000))
 while True:
-    # Gera um número aleatório de 0 a 10
-rand = random.randint(0, 10)
+    rand = random.randint(0, 10)  # Gera um número aleatório de 0 a 10
 # Recebe do cliente o pacote junto com seu endereço de destino
 message, address = serverSocket.recvfrom(1024)
 # Escreve a mensagem em letras maiúsculas
 message = message.upper()
 # Se rand < 4, consideramos que o pacote foi perdido
 if rand < 4:
-continue
+    continue
 # Caso contrário, o servidor responde
 serverSocket.sendto(message, address)
